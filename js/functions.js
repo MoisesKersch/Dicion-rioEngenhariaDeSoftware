18f5b1bdb3191c29a257935f0f1fc0f3886232eb
MoisesKersch <lightspeedoff@gmail.com>
2018-03-18 00:50:00 -0300

    Update

--- js/functions.js
+++ js/functions.js
@@ -19,3 +19,19 @@ function getDateTime()
 	document.getElementById('date').innerHTML = getDay(n.getDay()) + " " + getMonth(n.getMonth()) + " " + n.getDate() + ", " + n.getFullYear();	
 }
 
+function myFunction()
+{
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
